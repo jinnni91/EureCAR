e1f459435e8046b25198744f26eca0ba035619d3
sme1029 <seong161718@gmail.com>
2022-10-07 17:46:45 +0900

    자유게시판 10월 7일 3번째

---
+++ src/main/java/com/greedy/semi/free/dto/FreeReplyDTO.java
@@ -0,0 +1,21 @@
+package com.greedy.semi.free.dto;
+
+import java.sql.Date;
+
+import com.greedy.semi.member.dto.MemberDTO;
+
+import lombok.Data;
+
+@Data
+public class FreeReplyDTO {
+	
+    private Long replyNo;
+    private String replyContent;
+    private String replyDelete;
+    private Date replyDate;		            
+    private Date replyUpdate;		      
+    private Long freeNo;
+    private MemberDTO memberId;
+    private Long refreplyNo;
+
+}
---
+++ src/main/java/com/greedy/semi/free/entity/FreeReply.java
@@ -0,0 +1,67 @@
+package com.greedy.semi.free.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.DynamicInsert;
+
+import com.greedy.semi.member.entity.Member;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "FREE_REPLY")
+@SequenceGenerator(
+		name = "REPLY_SEQ_GENERATOR",
+		sequenceName = "SEQ_REPLY_NO",
+		initialValue = 1,
+		allocationSize = 1
+)
+@DynamicInsert 
+public class FreeReply {
+	
+	@Id
+	@Column(name = "REPLY_NO")
+	@GeneratedValue(
+			strategy = GenerationType.SEQUENCE,
+			generator = "REPLY_SEQ_GENERATOR"
+	)
+    private Long replyNo;
+	
+	@Column(name = "REFLY_CONTENT")
+    private Long reflyContent;
+	
+	@Column(name = "REPLY_DELETE")
+    private String replyDelete;
+	
+	@Column(name = "REPLY_DATE")
+    private Date replyDate;	
+	
+	@Column(name = "REPLY_UPDATE")
+    private Date replyUpdate;	
+
+	@Column(name = "FREE_NO")
+	private Long freeNo;
+	
+	@ManyToOne
+	@JoinColumn(name = "MEMBER_ID")
+	private Member memberId;
+	
+	@Column(name = "REF_REPLY_NO")
+	private Long refreplyNo;
+
+}
