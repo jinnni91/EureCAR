fc1b30ac013cb9c71bf4236423fa27f8fdba2b7f
dykim1996 <111603576+dykim1996@users.noreply.github.com>
2022-10-10 02:17:47 +0900

    Update MailConfig.java

--- src/main/java/com/greedy/semi/config/MailConfig.java
+++ src/main/java/com/greedy/semi/config/MailConfig.java
@@ -14,8 +14,8 @@ public class MailConfig {
         JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 
         javaMailSender.setHost("smtp.naver.com");
-        javaMailSender.setUsername("passby_"); //네이버 아이디
-        javaMailSender.setPassword("alwaysme");	//네이버 비밀번호
+        javaMailSender.setUsername(""); //네이버 아이디
+        javaMailSender.setPassword("");	//네이버 비밀번호
 
         javaMailSender.setPort(465);
 
