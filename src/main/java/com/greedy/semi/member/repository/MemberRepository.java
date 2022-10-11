21a31a0f3da06d5e108b8481d45417dd72cacb43
dykim1996 <111603576+dykim1996@users.noreply.github.com>
2022-10-12 00:35:19 +0900

    Update MemberRepository.java

--- src/main/java/com/greedy/semi/member/repository/MemberRepository.java
+++ src/main/java/com/greedy/semi/member/repository/MemberRepository.java
@@ -7,17 +7,16 @@ import com.greedy.semi.member.dto.MemberDTO;
 import com.greedy.semi.member.entity.Member;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-	
-	Optional<Member> findByMemberIdAndMemberStatus(String memberId, String memberStatus);
 
+	Optional<Member> findByMemberIdAndMemberStatus(String memberId, String memberStatus);
+	
+	Member findIdByNameAndEmailAndMemberStatus(String name, String email, String memberStatus);
+	
 	Member findIdByNameAndEmail(String name, String email);
 	
 	Member findByMemberId(String memberId);
 
-	Member findByMemberIdAndNameAndPhone(String memberId, String name, String phone);
-
-
-
+	Member findByMemberIdAndNameAndPhoneAndMemberStatus(String memberId, String name, String phone, String memberStatus);
 
 	
 
