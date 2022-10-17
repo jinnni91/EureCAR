550fc6952303cf2c86509d31e2953986b038acb5
현진 김 <jinnni919191@gmail.com>
2022-10-17 10:55:15 +0900

    최종적으로 고친 것 커밋

--- src/main/java/com/greedy/semi/mypage/controller/MypageController.java
+++ src/main/java/com/greedy/semi/mypage/controller/MypageController.java
@@ -35,107 +35,98 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("/mypage")
 public class MypageController {
 	
-	private final MessageSourceAccessor messageSourceAccessor;
-    private final MemberService memberService;
-	private final AuthenticationService authenticationService;
-	private final FreeService freeService;
-	private final PasswordEncoder passwordEncoder;
-	
-	public MypageController(MessageSourceAccessor messageSourceAccessor, 
-			                MemberService memberService, 
-			                AuthenticationService authenticationService, 
-			                FreeService freeService,
-			                PasswordEncoder passwordEncoder) {
-		
-		this.authenticationService = authenticationService;
-        this.memberService = memberService;
-        this.messageSourceAccessor = messageSourceAccessor;
-        this.freeService = freeService;
-        this.passwordEncoder = passwordEncoder;
-	}	
-	
-	@GetMapping("/mypage")
-	public String myPage() {
-		
-		return "mypage/mypage";
-	}
-	
-	@GetMapping("/mylist")
-	public String myList(@RequestParam(defaultValue="1") int page,
-			             @RequestParam(required=false) String searchValue,
-			             Model model) {
-		
-		Page<FreeDTO> list = freeService.selectList(page, searchValue);
-		PagingButtonInfo paging = Pagenation.getPagingButtonInfo(list);
-		
-		model.addAttribute("list", list);
-		model.addAttribute("paging", paging);
-		
-		if(searchValue != null && !searchValue.isEmpty()) {
-			
-			model.addAttribute("searchValue", searchValue);
-		}
-		  
-		return "mypage/mylist";
-	}
-	
-	@GetMapping("/delete")
-	public String deleteMember() {
-		
-	    return "mypage/delete";
-	}
-	
-	@PostMapping("/delete")
-	public String deleteMember(@AuthenticationPrincipal MemberDTO member,
-			                   RedirectAttributes rttr) {
-		
-		memberService.removeMember(member);
-		
-		SecurityContextHolder.clearContext();
-
-        rttr.addFlashAttribute("message", messageSourceAccessor.getMessage("member.delete"));
-		
-		return "redirect:/";
-	}
-	
-	@GetMapping("/update")
-	public String goModifyMember() {
-		
-		return "mypage/update";
-	}
-	
-	@PostMapping("/update")
-    public String modifyMember(@ModelAttribute MemberDTO updateMember,
-	    					   @RequestParam String zipCode, 
-	    					   @RequestParam String address1, 
-	    				       @RequestParam String address2,
-	    				       @AuthenticationPrincipal MemberDTO loginMember,
-	    					   RedirectAttributes rttr) {
-		
-    	String address = zipCode + "$" + address1 + "$" + address2;
-    	
-    	updateMember.setAddress(address);
-    	updateMember.setPhone(updateMember.getPhone().replace("-", ""));
-    	updateMember.setMemberId(loginMember.getMemberId());
-    	
-    	memberService.modifyMember(updateMember);
-    	
-    	Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    	SecurityContextHolder.getContext().setAuthentication(createNewAuthentication(authentication, loginMember.getMemberId()));
-    	
-    	rttr.addFlashAttribute("message", messageSourceAccessor.getMessage("member.modify"));
-    	
-		return "redirect:/";
-		
-	}
-	
-	protected Authentication createNewAuthentication(Authentication currentAuth, String memberId) {
-	    	
-	    UserDetails newPrincipal = authenticationService.loadUserByUsername(memberId);
-	    UsernamePasswordAuthenticationToken newAuth = new UsernamePasswordAuthenticationToken(newPrincipal, currentAuth.getCredentials(), newPrincipal.getAuthorities());
-	    newAuth.setDetails(currentAuth.getDetails());
-	    return newAuth;
+	 private final MessageSourceAccessor messageSourceAccessor;
+	    private final MemberService memberService;
+	    private final AuthenticationService authenticationService;
+	    private final FreeService freeService;
+	    private final PasswordEncoder passwordEncoder;
+	    
+	    public MypageController(MessageSourceAccessor messageSourceAccessor, 
+	                            MemberService memberService, 
+	                            AuthenticationService authenticationService, 
+	                            FreeService freeService,
+	                            PasswordEncoder passwordEncoder) {
 	        
+	        this.authenticationService = authenticationService;
+	        this.memberService = memberService;
+	        this.messageSourceAccessor = messageSourceAccessor;
+	        this.freeService = freeService;
+	        this.passwordEncoder = passwordEncoder;
+	    }   
+	    
+	    @GetMapping("/mypage")
+	    public String myPage() {
+	        
+	        return "mypage/mypage";
+	    }
+	    
+	    @GetMapping("/mylist")
+	    public String myList(@RequestParam(defaultValue="1") int page,
+	                         @RequestParam(required=false) String searchValue,
+	                         Model model) {
+	        
+	        Page<FreeDTO> list = freeService.selectList(page, searchValue);
+	        PagingButtonInfo paging = Pagenation.getPagingButtonInfo(list);
+	        
+	        model.addAttribute("list", list);
+	        model.addAttribute("paging", paging);
+	        
+	        if(searchValue != null && !searchValue.isEmpty()) {
+	            
+	            model.addAttribute("searchValue", searchValue);
+	        }
+	          
+	        return "mypage/mylist";
+	    }
+	    
+	    @GetMapping("/delete")
+	    public String deleteMember(@AuthenticationPrincipal MemberDTO member, 
+	                               RedirectAttributes rttr) {
+	        memberService.removeMember(member);
+	        SecurityContextHolder.clearContext();
+	        rttr.addFlashAttribute("message", messageSourceAccessor.getMessage("member.delete"));
+	    
+	        return "redirect:/";
+	    }
+	    
+	    @GetMapping("/update")
+	    public String goModifyMember() {
+	        
+	        return "mypage/update";
+	    }
+	    
+	    @PostMapping("/update")
+	    public String modifyMember(@ModelAttribute MemberDTO updateMember,
+	                               @RequestParam String zipCode, 
+	                               @RequestParam String address1, 
+	                               @RequestParam String address2,
+	                               @AuthenticationPrincipal MemberDTO loginMember,
+	                               RedirectAttributes rttr) {
+	        
+	        String address = zipCode + "$" + address1 + "$" + address2;
+	        
+	        updateMember.setAddress(address);
+	        updateMember.setPhone(updateMember.getPhone().replace("-", ""));
+	        updateMember.setMemberId(loginMember.getMemberId());
+	        
+	        memberService.modifyMember(updateMember);
+	        
+	        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+	        SecurityContextHolder.getContext().setAuthentication(createNewAuthentication(authentication, loginMember.getMemberId()));
+	        
+	        rttr.addFlashAttribute("message", messageSourceAccessor.getMessage("member.modify"));
+	        
+	        return "redirect:/";
+	        
+	    }
+	    
+	    protected Authentication createNewAuthentication(Authentication currentAuth, String memberId) {
+	            
+	        UserDetails newPrincipal = authenticationService.loadUserByUsername(memberId);
+	        UsernamePasswordAuthenticationToken newAuth = new UsernamePasswordAuthenticationToken(newPrincipal, currentAuth.getCredentials(), newPrincipal.getAuthorities());
+	        newAuth.setDetails(currentAuth.getDetails());
+	        return newAuth;
+	            
+	    }
+	    
 	}
-	
-}
